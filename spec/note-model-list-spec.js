62275325c885d0ca7e23abfd28028af5f206e39f
micawhitby <micawhitby@gmail.com>
2017-02-14 12:06:36 +0000

    latest version

--- spec/note-model-list-spec.js
+++ spec/note-model-list-spec.js
@@ -1,9 +1,19 @@
-// the list stores an array of notes. Has a method that returns the notes
 
+// the list stores an array of notes. Has a method that returns the notes
 function listHasArrayOfNotes() {
   var newList = new List();
   assert.isTrue(Array.isArray(newList.notes()));
 }
-
 listHasArrayOfNotes()
+
+
+
 // list has a method that creates and stores a new single note
+function listCanCreateAndStoreNotes() {
+  var newList = new List();
+  newList.addNote("This is my note");
+  newList.addNote("This is my other note");
+  console.log(newList._notes)
+  assert.isTrue(newList.notes().includes("This is my note") && newList.notes().includes("This is my other note"));
+};
+listCanCreateAndStoreNotes()
--- src/note-model-list.js
+++ src/note-model-list.js
@@ -8,5 +8,9 @@
     return this._notes;
   };
 
+  List.prototype.addNote = function(text){
+    this._notes.push(new Note(text));
+  };
+
   exports.List = List
 })(this);
