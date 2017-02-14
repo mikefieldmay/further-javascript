383bea72744c3068d3b4421066d76e9cb2701d7d
Tamar Lehmann <tlehmann@hotmail.co.uk>
2017-02-14 12:56:57 +0000

    Made tests for list note view pass and added tests for different list lengths

--- spec/note-list-view-spec.js
+++ spec/note-list-view-spec.js
@@ -8,3 +8,34 @@ function viewDisplaysListOfNotes() {
 }
 
 viewDisplaysListOfNotes()
+
+function viewDisplaysASingleNote() {
+  var newList = new List();
+  newList.addNote("This is my note");
+  var listView = new ListView(newList);
+  var htmlString = '<ul><li><div>This is my note</div></li></ul>'
+  assert.isTrue(listView.viewNotes() === htmlString);
+}
+
+viewDisplaysASingleNote()
+
+function viewDisplaysLongListOfNotes() {
+  var newList = new List();
+  newList.addNote("This is my note");
+  newList.addNote("This is my other note");
+  newList.addNote("This is my third note");
+  var listView = new ListView(newList);
+  var htmlString = '<ul><li><div>This is my note</div></li><li><div>This is my other note</div></li><li><div>This is my third note</div></li></ul>'
+  assert.isTrue(listView.viewNotes() === htmlString);
+}
+
+viewDisplaysLongListOfNotes()
+
+function viewDisplaysNothingWhenNoNotes() {
+  var newList = new List();
+  var listView = new ListView(newList);
+  var htmlString = '<ul></ul>'
+  assert.isTrue(listView.viewNotes() === htmlString);
+}
+
+viewDisplaysNothingWhenNoNotes()
---
+++ src/note-list-view.js
@@ -0,0 +1,15 @@
+(function(exports){
+  function ListView(list) {
+    this._list = list.notes()
+  }
+
+  ListView.prototype.viewNotes = function(){
+    var string = ""
+      for(i = 0; i < this._list.length; i++) {
+       string +=  "<li><div>" + this._list[i].text() + "</div></li>"
+      }
+    return "<ul>" + string + "</ul>"
+  }
+
+exports.ListView = ListView;
+})(this);
